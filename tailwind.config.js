ab7ee81d0f9e72c31d3bc95da295cf9733f92b79
MeghlaB <meghlab964@gmail.com>
2025-01-23 15:24:46 +0600

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,8 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html",
   ],
   theme: {
     extend: {
